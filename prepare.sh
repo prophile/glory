92715bc4b20eddd4a9b2098870e23660acb29c12
Alastair Lynn <arplynn@gmail.com>
2013-09-23 17:29:02 +0100

    Decrypt in the preparation process

--- prepare.sh
+++ prepare.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 git submodule update --init
 wget 'http://ci.md-5.net/job/Spigot/lastStableBuild/artifact/Spigot-Server/target/spigot.jar' -O modules/minecraft/files/spigot.jar
+scripts/decrypt.sh
 
