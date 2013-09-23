384c5f736f0cc1ed5a56e3bc5453ed9784ee31b9
Alastair Lynn <arplynn@gmail.com>
2013-09-23 17:34:28 +0100

    Do secrets decryption first in prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+scripts/decrypt.sh
 git submodule update --init
 wget 'http://ci.md-5.net/job/Spigot/lastStableBuild/artifact/Spigot-Server/target/spigot.jar' -O modules/minecraft/files/spigot.jar
-scripts/decrypt.sh
 
