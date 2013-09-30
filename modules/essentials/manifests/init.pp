00ff17f3aa5438b58f3b30ea60e563684b257372
Alastair Lynn <arplynn@gmail.com>
2013-09-30 21:51:08 +0100

    Remove some permissions

--- modules/essentials/manifests/init.pp
+++ modules/essentials/manifests/init.pp
@@ -1,6 +1,24 @@
 class essentials {
   minecraft::plugin {"Essentials":
-    source => "puppet:///modules/essentials/Essentials.jar"
+    source  => "puppet:///modules/essentials/Essentials.jar",
+    require => File["${minecraft::homedir}/plugins/Essentials/config.yml"]
+  }
+
+  file {"${minecraft::homedir}/plugins/Essentials":
+    ensure  => directory,
+    owner   => $minecraft::user,
+    group   => $minecraft::group,
+    mode    => '0755',
+    require => File["${minecraft::homedir}/plugins"]
+  }
+
+  file {"${minecraft::homedir}/plugins/Essentials/config.yml":
+    ensure  => file,
+    owner   => $minecraft::user,
+    group   => $minecraft::group,
+    mode    => '0644',
+    content => template('essentials/config.yml.erb'),
+    require => File["${minecraft::homedir}/plugins/Essentials"]
   }
 }
 
