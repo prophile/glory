358a17b2145c203e0eac2562ed90d7d1c3e910a8
Alastair Lynn <arplynn@gmail.com>
2013-10-14 17:20:02 +0100

    Fix #29: Implement restore script

--- modules/longbackup/manifests/init.pp
+++ modules/longbackup/manifests/init.pp
@@ -7,8 +7,22 @@ class longbackup {
         content => template('longbackup/mcbackup.erb')
     }
 
+    package { 'realpath':
+        ensure => present
+    }
+
+    file { '/usr/local/bin/mc-restore':
+        ensure  => file,
+        owner   => 'root',
+        group   => 'staff',
+        mode    => '0755',
+        content => template('longbackup/restore.erb'),
+        require => Package['realpath']
+    }
+
     file_line {"cron":
-        require => File["${minecraft::homedir}/scripts.txt"],
+        require => [File["${minecraft::homedir}/scripts.txt"],
+                    File['/usr/local/bin/mcbackup']],
         path    => "${minecraft::homedir}/scripts.txt",
         ensure  => present,
         line    => '8 12 * * * $mcbackup'
