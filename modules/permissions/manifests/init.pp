4f25a89b39b7f4580fab326925a9135710bcdd23
Alastair Lynn <arplynn@gmail.com>
2013-10-17 12:19:31 +0100

    Make ops.txt an alias for admins.txt

--- modules/permissions/manifests/init.pp
+++ modules/permissions/manifests/init.pp
@@ -7,5 +7,14 @@ class permissions {
         mode   => '0644',
         path   => "${minecraft::homedir}/admins.txt",
     }
+
+    file { "ops.txt":
+        owner  => $minecraft::user,
+        group  => $minecraft::group,
+        mode   => '0644',
+        ensure => link,
+        path   => "${minecraft::homedir}/ops.txt",
+        target => "${minecraft::homedir}/admins.txt",
+    }
 }
 
