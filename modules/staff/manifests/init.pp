be41742a90530f4c1ad39fd30910c1fbb56e3417
Alastair Lynn <arplynn@gmail.com>
2013-10-17 17:56:58 +0100

    Fix #44: add core user permissions

--- modules/staff/manifests/init.pp
+++ modules/staff/manifests/init.pp
@@ -13,5 +13,28 @@ class staff {
     permissions::mod_permission {
         'prism.*':;
     }
+
+    permissions::user_permission {
+        # Essentials commands that do not give out extra information
+        'essentials.compass':;
+        'essentials.depth':;
+        'essentials.getpos':;
+        'essentials.itemdb':;
+        'essentials.list':;
+        'essentials.motd':;
+        'essentials.seen':;
+        'essentials.rules':;
+        'essentials.recipe':;
+        # Potentially useful
+        'essentials.suicide':;
+        'essentials.ping':;
+        # Private messaging, and public messaging
+        'essentials.ignore':;
+        'essentials.me':;
+        'essentials.msg':;
+        'essentials.msg.url':;
+        # Help pages
+        'essentials.help':;
+    }
 }
 
