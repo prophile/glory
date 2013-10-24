e587345170fb2e446d9c4b4a7a5c18656a17d234
Alastair Lynn <arplynn@gmail.com>
2013-10-24 20:30:11 +0100

    Add the skeleton of the new secrets system

---
+++ scripts/unpack-secrets.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+
+require "erb"
+require "yaml"
+
+file = File.read('settings.yaml')
+settings = YAML::load(file)
+
+erb = ERB.new File.read('scripts/skeleton-site.erb')
+File.open('site.pp', 'w') do |handle|
+  handle.write erb.result
+end
+
