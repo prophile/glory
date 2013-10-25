8e3c81b3f15ea3f8d2f9dda9ee758f1b51b8ff50
Alastair Lynn <arplynn@gmail.com>
2013-10-25 16:09:32 +0100

    Fix #65: Error out if no settings.yaml

--- scripts/unpack-secrets.rb
+++ scripts/unpack-secrets.rb
@@ -3,11 +3,16 @@
 require "erb"
 require "yaml"
 
-file = File.read('settings.yaml')
-settings = YAML::load(file)
+begin
+  file = File.read('settings.yaml')
+  settings = YAML::load(file)
 
-erb = ERB.new File.read('scripts/skeleton-site.erb')
-File.open('manifests/base.pp', 'w') do |handle|
-  handle.write erb.result
+  erb = ERB.new File.read('scripts/skeleton-site.erb')
+  File.open('manifests/base.pp', 'w') do |handle|
+    handle.write erb.result
+  end
+rescue Errno::ENOENT
+  puts "No settings.yaml file found."
+  exit 1
 end
 
