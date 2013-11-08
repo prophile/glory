8d22996a6b2c787e7708f8e652bebdbef74605bb
Alastair Lynn <arplynn@gmail.com>
2013-11-08 09:57:34 +0000

    Fix #111: Move the conditional logic to the longbackup module
    longbackup is now included whether or not the S3 backups are being
    used. This is because the mc-restore script belongs in longbackup and
    is needed whether or not S3 is.

--- modules/longbackup/manifests/init.pp
+++ modules/longbackup/manifests/init.pp
@@ -1,12 +1,4 @@
-class longbackup($bucket) {
-    file { '/usr/local/bin/mcbackup':
-        ensure  => file,
-        owner   => 'root',
-        group   => 'staff',
-        mode    => '0755',
-        content => template('longbackup/mcbackup.erb')
-    }
-
+class longbackup($bucket = '') {
     package { 'realpath':
         ensure => present
     }
@@ -20,13 +12,32 @@ class longbackup($bucket) {
         require => Package['realpath']
     }
 
-    cron { "long-backup":
-        require => [File['/usr/local/bin/mcbackup'],
-                    User['minecraft']],
-        minute  => '8',
-        hour    => '4',
-        command => '/usr/local/bin/mcbackup',
-        user    => 'minecraft',
+    if $bucket != '' {
+        cron { "long-backup":
+            require => [File['/usr/local/bin/mcbackup'],
+                        User['minecraft']],
+            minute  => '8',
+            hour    => '4',
+            command => '/usr/local/bin/mcbackup',
+            user    => 'minecraft',
+            ensure  => present
+        }
+
+        file { '/usr/local/bin/mcbackup':
+            ensure  => file,
+            owner   => 'root',
+            group   => 'staff',
+            mode    => '0755',
+            content => template('longbackup/mcbackup.erb')
+        }
+    } else {
+        cron { "long-backup":
+            ensure => absent
+        }
+
+        file { '/usr/local/bin/mcbackup':
+            ensure  => absent
+        }
     }
 }
 
