fc7d6fcf6b58c06d8a116424c668c5289eb7193d
Alastair Lynn <arplynn@gmail.com>
2013-11-09 14:33:36 +0000

    Fix #118: Have WorldGuard overrule vanilla protection

--- modules/worldguard/manifests/init.pp
+++ modules/worldguard/manifests/init.pp
@@ -2,5 +2,10 @@ class worldguard {
   minecraft::plugin {"WorldGuard":
     source => "puppet:///modules/worldguard/WorldGuard.jar"
   }
+
+  # Turn off vanilla spawn protection
+  minecraft::server_prop { "spawn-protection":
+    value => 0;
+  }
 }
 
